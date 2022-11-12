6851e6d44b8ddc59befbb1d47b1788bbc454c84d
Sima <tarverdans15@gmail.com>
2022-11-12 15:30:17 +0300

    alldone

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express')
+const cors = require('cors')
 
 const token = '5679319822:AAEdpnGEsgaW1ofjFwbtj7h8NihRw9dn6RU';
 const webAppUrl = 'https://heroic-empanada-97d0d5.netlify.app'
 const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
@@ -12,7 +18,7 @@ bot.on('message', async (msg) => {
     await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
         reply_markup: {
             keyboard:[
-                [{text: 'Заполнить форму '}]
+                [{text: 'Заполнить форму ', web_app: {url: webAppUrl + '/form'}}]
             ]
         }
     })
@@ -25,6 +31,43 @@ bot.on('message', async (msg) => {
     })
   }
 
-  
-  bot.sendMessage(chatId, 'Received your message');
+  if(msg?.web_app_data?.data) {
+    try {
+        const data = JSON.parse(msg?.web_app_data?.data)
+
+        await bot.sendMessage(chatId,'Спасибо за обратную связь! ')
+        await bot.sendMessage(chatId,'Ваша страна ' + data?.country);
+        await bot.sendMessage(chatId,'Ваша улица ' + data?.street);
+
+        setTimeout(async () => {
+            await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате ');
+
+        }, 3000)
+        
+    } catch(e){
+        conlose.log(e);
+    }
+    
+  }
 });
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
