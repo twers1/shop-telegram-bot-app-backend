985dd034557ea031b9c03806521f5a88d08f69fc
Sima <tarverdans15@gmail.com>
2022-11-15 09:37:39 +0300

    editform

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ bot.on('message', async (msg) => {
         const data = JSON.parse(msg?.web_app_data?.data)
 
         await bot.sendMessage(chatId,'Спасибо за обратную связь! ')
-        await bot.sendMessage(chatId,'Ваша страна ' + data?.country);
-        await bot.sendMessage(chatId,'Ваша улица ' + data?.street);
+        await bot.sendMessage(chatId,'Ваше ФИО: ' + data?.fio);
+        await bot.sendMessage(chatId,'Ваша улица: ' + data?.street);
 
         setTimeout(async () => {
             await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате ');
